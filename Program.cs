3d7f641ccbdcf19c4e3dac9b7a630a68c2f9192d
gangai22 <gangaidzotich123@gmail.com>
2021-10-13 14:11:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
